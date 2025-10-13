d082ce93c9a883765893ec067f5819e8093fec52
Tavi Tonciu <octavian.tonciu@stud.fiir.upb.ro>
2025-10-13 15:49:34 +0300

    Push proiect 2

---
+++ partials/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?> - Magazinul de Muzică</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="index.php" class="logo">🎵 Magazinul de Muzică</a>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Acasă</a></li>
+                    <li><a href="chitare.php">Chitare</a></li>
+                    <li><a href="clape.php">Clape</a></li>
+                    <li><a href="percutie.php">Percuție</a></li>
+                    <li><a href="instrumente_de_suflat.php">Suflători</a></li>
+                    <li><a href="corzi.php">Corzi</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="container"></div>
